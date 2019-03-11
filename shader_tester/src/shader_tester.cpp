1e7ad0aca4e3b4d91d03fd38bcc5d95adddebc16
Aaron Golliver <agolliver@tableau.com>
2019-03-11 15:20:21 -0700

    use new live reloading shader

--- shader_tester/src/shader_tester.cpp
+++ shader_tester/src/shader_tester.cpp
@@ -3,15 +3,11 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <chrono>
+#include <memory>
 #include <filesystem>
-#include <SFML/Graphics.hpp>
+#include <unordered_map>
 
-#include "shader_header.h"
-#include "file_watcher.h"
-
-static auto startT = std::chrono::steady_clock::now();
+#include "live_reloading_shader.h"
 
 #ifdef _EXPERIMENTAL_FILESYSTEM_
 namespace fs = std::experimental::filesystem;
@@ -22,88 +18,18 @@ namespace fs = std::filesystem;
 using namespace std::chrono_literals;
 
 void _main(std::vector<std::string> args) {
-    SimpleFileWatcher fw(args[1]);
-    sf::RenderWindow window({ 512, 512}, "shader toy", sf::Style::Resize);
-    window.setVerticalSyncEnabled(true);
-    window.setFramerateLimit(0);
-
-    sf::Shader shader;
+    std::unordered_map<std::string, std::unique_ptr<LiveReloadingShader>> shaders;
     for (const auto & shaderFile : fs::directory_iterator(args[1])) {
-        bool success = false;
-        do {
-            success = shader.loadFromMemory(shaderHeader + LoadFile(shaderFile.path().generic_string()), sf::Shader::Type::Fragment);
-            if (!success) {
-                std::cerr << "FAILED TO LOAD SHADER " << std::endl;
-                std::this_thread::sleep_for(100ms);
-                ClearCmd();
-            }
-        } while (!success);
-        ClearCmd();
-        std::cout << "LOADED" << std::endl;
-    }
-    
-
-    sf::RenderTexture tex;
-    tex.create(window.getSize().x, window.getSize().y);
-    sf::Glsl::Vec2 mousePos = { window.getSize().x/2.f, window.getSize().y/2.f };
-    while (window.isOpen()) {
-        window.clear();
-        tex.clear();
-        const auto& size = window.getSize();
-        shader.setUniform("iTime", (std::chrono::steady_clock::now() - startT).count() * 1.f / std::chrono::steady_clock::period::den);
-        shader.setUniform("iResolution", sf::Glsl::Vec2(size));
-        shader.setUniform("iMouse", mousePos);
-
-        const auto& currentTexture = tex.getTexture();
-        sf::Sprite currentSprite(currentTexture);
-        sf::RenderStates renderStates(&shader);
-        renderStates.blendMode = { sf::BlendMode::One, sf::BlendMode::One };
-        tex.draw(currentSprite, renderStates);
-        tex.display();
-        window.draw(currentSprite);
-
-        window.display();
-        static bool mouseEnabled = false;
-        sf::Event e;
-        while (window.pollEvent(e)) {
-            if (e.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (e.type == sf::Event::KeyReleased) {
-                if (e.key.code == sf::Keyboard::Key::Escape) {
-                    window.close();
-                }
-            }
-            if (e.type == sf::Event::Resized) {
-                window.setView({ {e.size.width / 2.f, e.size.height / 2.f}, {1.f*e.size.width, 1.f*e.size.height} });
-                tex.create(e.size.width, e.size.height);
-            }
-            if (e.type == sf::Event::MouseButtonPressed && e.mouseButton.button == sf::Mouse::Button::Left)
-                mouseEnabled = true;
-            if (e.type == sf::Event::MouseButtonReleased && e.mouseButton.button == sf::Mouse::Button::Left)
-                mouseEnabled = false;
-            if (e.type == sf::Event::MouseMoved) {
-                if(mouseEnabled)
-                mousePos = { e.mouseMove.x * 1.f, e.mouseMove.y *  1.f };
-            }
-        }
-        if (fw.CheckChanged()) {
-            for (const auto & shaderFile : fs::directory_iterator(args[1])) {
-                bool success = false;
-                do {
-                    success = shader.loadFromMemory(shaderHeader + LoadFile(shaderFile.path().generic_string()), sf::Shader::Type::Fragment);
-                    if (!success) {
-                        std::cerr << "FAILED TO LOAD SHADER " << std::endl;
-                        std::this_thread::sleep_for(100ms);
-                        ClearCmd();
-                    }
-                } while (!success);
-                ClearCmd();
-                std::cout << "LOADED" << std::endl;
-            }
-        }
+        shaders.emplace(shaderFile.path().generic_string(), std::make_unique<LiveReloadingShader>(shaderFile.path()));
     }
 
+    while (1) {
+        for (const auto& shader : shaders) {
+            shader.second->Tick();
+            if (shader.second->windowClosed)
+                return;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
