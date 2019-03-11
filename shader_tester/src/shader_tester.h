0c18d947b7754fca967902435af2fef4535409c2
Aaron Golliver <agolliver@tableau.com>
2019-03-11 11:26:12 -0700

    move into an unnamed namespace

--- shader_tester/src/shader_tester.h
+++ shader_tester/src/shader_tester.h
@@ -3,31 +3,34 @@
 #include <FileWatcher/FileWatcher.h>
 #include <windows.h>
 
-template<typename T>
-class LambdaWatcher : public FW::FileWatchListener {
-    const T _func;
-public:
-    LambdaWatcher(T& func) : _func(func) {
-    }
-    ~LambdaWatcher() override = default;
-    void handleFileAction(FW::WatchID, const FW::String&, const FW::String&, FW::Action) override {
-        _func();
-    }
-};
+namespace {
 
-void clearcmd() {
-    COORD topLeft = { 0, 0 };
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO screen;
-    DWORD written;
+    template<typename T>
+    class LambdaWatcher : public FW::FileWatchListener {
+        const T _func;
+    public:
+        LambdaWatcher(T& func) : _func(func) {
+        }
+        ~LambdaWatcher() override = default;
+        void handleFileAction(FW::WatchID, const FW::String&, const FW::String&, FW::Action) override {
+            _func();
+        }
+    };
 
-    GetConsoleScreenBufferInfo(console, &screen);
-    FillConsoleOutputCharacterA(
-        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-    );
-    FillConsoleOutputAttribute(
-        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-    );
-    SetConsoleCursorPosition(console, topLeft);
+    void ClearCmd() {
+        COORD topLeft = { 0, 0 };
+        HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO screen;
+        DWORD written;
+
+        GetConsoleScreenBufferInfo(console, &screen);
+        FillConsoleOutputCharacterA(
+            console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+        );
+        FillConsoleOutputAttribute(
+            console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+            screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+        );
+        SetConsoleCursorPosition(console, topLeft);
+    }
 }
