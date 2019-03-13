28ee7102bddd096ffbe3a99ebdd80d02f119c14e
Aaron Golliver <aaron.golliver@gmail.com>
2019-03-12 23:59:42 -0700

    work on making progress bar error quicker

--- examples/progress_bar/progress_bar.glsl
+++ examples/progress_bar/progress_bar.glsl
@@ -1,17 +1,17 @@
+// this progress gets blurier and blurier as it gets closer to the top because of additave AA errors
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     bool reset = false;
+    vec4 c = vec4(1);
     if (reset) {
-        fragColor = vec4(0,0,0,1);
+        c = vec4(0);
+        fragColor = c;
     } else {
-        if(fragCoord.x < 10 && fragCoord.y < 10) {
-            fragColor = vec4(1,1,1,1);
+        if(fragCoord.y < 1) {
+            float new_c = (1 - texture2D(texture_progress_bar, vec2(.5, 1)).r);
+            fragColor = vec4(abs(new_c));
         } else {
-            if(fragCoord.x <= 1) {
-                fragColor = vec4(texture2D(texture_progress_bar, vec2(1, (fragCoord.y - 10) / iResolution.x)).rgb, 1);
-            } else {
-                fragColor = vec4(texture2D(texture_progress_bar, vec2((fragCoord.x-10)/iResolution.x, (fragCoord.y)/iResolution.y)).rgb, 1);
-            }
+            fragColor = vec4(texture2D(texture_progress_bar, vec2((fragCoord.x)/iResolution.x, (fragCoord.y-iTimeDelta*300)/iResolution.y)).rgb, 1);
         }
     }
 }
