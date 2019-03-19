5180a569a87301ca0d0092967054abe5f47f658e
Aaron Golliver <aaron.golliver@gmail.com>
2019-03-18 22:39:30 -0700

    make sheet2 a scatter plot

--- examples/data/sheet2.glsl
+++ examples/data/sheet2.glsl
@@ -10,20 +10,26 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec3 color = texture(texture_color_picker, vec2(0,0)).rgb;
     float size = texture(texture_size, vec2(0,0)).x;
 
-    #define chunks 5
+    #define points 100
     #define PI 3.141590
-    float positions[chunks] = {.1, 1, 2, 3, 4};
 
-    float prevTheta = 0;
-    for(int i = 0; i < chunks; ++i) {
-        float theta = positions[i] - .1;
-        while(theta > 2*PI) { 
-            theta -= 2*PI;
-        }
-        float fragTheta = atan(frag.y + .5, frag.x + .5);
-        if(fragTheta > prevTheta && fragTheta < theta)
-            fragColor = vec4(color, 1);
-
-        prevTheta = theta;
+    for(int i = 0; i < points; ++i) {
+        float posX = abs(sin(i*1514+7));
+        float posY = abs(sin(i*6554+5));
+        vec2 pos = vec2(posX, posY);
+        vec2 dist = frag - pos;
+        float d = sqrt(dist.x * dist.x + dist.y * dist.y);
+        if(d < size/10)
+        fragColor = vec4(color, 1);
     }
+
+    if(frag.x < .02)
+    fragColor = vec4(.5);
+    if(frag.y < .02)
+    fragColor = vec4(.5);
+    if(frag.x < .01)
+    fragColor = vec4(1);
+    if(frag.y < .01)
+    fragColor = vec4(1);
+
 }
